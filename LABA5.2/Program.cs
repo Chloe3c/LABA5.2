d4ffc518bcab232bf10085469ab2c2d54674cb33
Chloe3c <kristianishenko21@gmail.com>
2024-11-30 13:44:32 +0200

    Добавьте файлы проекта.

---
+++ LABA5.2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Для заданной строки определить входит ли в нее хотя бы 1 латинская буква
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        bool containsLatinLetter = input.Any(c => char.IsLetter(c) &&
+                                     (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z'));
+        if (containsLatinLetter)
+        {
+            Console.WriteLine("Строка содержит хотя бы одну латинскую букву.");
+        }
+        else
+        {
+            Console.WriteLine("Строка не содержит латинских букв.");
+        }
+    }
+}
